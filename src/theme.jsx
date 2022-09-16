463e47ef1c8874ccbe2d31a95cc514ef45cb7087
Andrey1914 <anlikajw@gmail.com>
2022-09-16 15:13:35 +0200

    created theme.jsx, GlobalStyles.jsx, redux folder, components folders, Box element

---
+++ src/theme.jsx
@@ -0,0 +1,45 @@
+export const theme = {
+  colors: {
+    black: '#000',
+    white: '#fff',
+    text: '#000',
+    textHover: '#fff',
+    background: '#fff',
+    backdrop: 'rgba(0, 0, 0, 0.8)',
+    primary: '#07c',
+    secondary: '#05a',
+    accent: '#609',
+    muted: '#ccc',
+    lightMuted: '#ededed',
+  },
+  space: [0, 2, 4, 8, 16, 20, 32, 64, 128, 152, 256, 320, 512],
+  fonts: {
+    body: 'Roboto, Oxygen, sans-serif',
+    heading: 'Roboto, Oxygen, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: [12, 14, 16, 32, 64],
+  fontWeigths: [400, 500, 700],
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  borders: {
+    none: 'none',
+    normal: '1px solid #ccc',
+  },
+  radii: {
+    none: '0',
+    small: '2px',
+    normal: '4px',
+    big: '8px',
+    round: '50%',
+  },
+  //   breakpoints: [ '40em', '52em', '64em' ],
+
+  // mediaQueries: {
+  //   // small: `@media screen and (min-width: ${breakpoints[0]})`,
+  //   medium: `@media screen and (min-width: 768px)`,
+  //   large: `@media screen and (min-width: 1200px)`,
+  // },
+};
